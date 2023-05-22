362dafcc8f5f158e87336025e8c4100df772596d
Joao Galeao <jbgaleao@gmail.com>
2023-05-22 14:56:48 -0300

    Add project files.

---
+++ Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AgendaTarefas.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> TAREFAS { get; set; }
+
+    }
+}
---
+++ Models/Tarefa.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaTarefas.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "Campo {0} não deve ultreapassar os 50 caracteres")]
+        public string Nome { get; set; }
+
+        public string Data { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        [DataType(DataType.Time)]
+        public string Horario { get; set; }
+    }
+}
